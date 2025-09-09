73982267b85048de1d5bb29f799b38cd5fde61d9
John Anigbogu, Jr. <johnoaj11@gmail.com>
2025-09-09 17:15:55 -0400

    2 commit

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.simplecounter
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val UpgrBtn = findViewById<Button>(R.id.UpgrdeBtn)
+
+
+        button.setOnClickListener {
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100){
+                UpgrBtn.visibility = View.VISIBLE
+                UpgrBtn.setOnClickListener {
+                    button.text = "Add 2"
+
+                    button.setOnClickListener {
+                        counter+=2
+                        textView.text = counter.toString()
+                    }
+                    UpgrBtn.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
